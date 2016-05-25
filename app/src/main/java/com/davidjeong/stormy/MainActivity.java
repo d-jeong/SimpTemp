2795a89c706322413ee8fa7e6ae9eaff395d8986
yechanjeong <yechanjeong@yahoo.com>
2016-05-25 19:00:17 -0400

    GPS location working with a bug. BUG: must press refresh to get location. Otherwise, it defaults to 0,0

--- app/src/main/java/com/davidjeong/stormy/MainActivity.java
+++ app/src/main/java/com/davidjeong/stormy/MainActivity.java
@@ -1,10 +1,15 @@
 package com.davidjeong.stormy;
 
 import android.content.Context;
+import android.content.IntentSender;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,10 +18,18 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,20 +39,42 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     private CurrentWeather mCurrentWeather;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
-    @BindView(R.id.timeLabel) TextView mTimeLabel;
-    @BindView(R.id.temperatureLabel) TextView mTemperatureLabel;
-    @BindView(R.id.humidityValue) TextView mHumidityValue;
-    @BindView(R.id.precipValue) TextView mPrecipValue;
-    @BindView(R.id.summaryLabel) TextView mSummaryLabel;
-    @BindView(R.id.iconImageView) ImageView mIconImageView;
-    @BindView(R.id.refreshImageView) ImageView mRefreshImageView;
-    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    private double mLatitude;
+    private double mLongitude;
+    private String mCity;
+    private String mState;
+
+    @BindView(R.id.locationLabel)
+    TextView mLocationLabel;
+    @BindView(R.id.timeLabel)
+    TextView mTimeLabel;
+    @BindView(R.id.temperatureLabel)
+    TextView mTemperatureLabel;
+    @BindView(R.id.humidityValue)
+    TextView mHumidityValue;
+    @BindView(R.id.precipValue)
+    TextView mPrecipValue;
+    @BindView(R.id.summaryLabel)
+    TextView mSummaryLabel;
+    @BindView(R.id.iconImageView)
+    ImageView mIconImageView;
+    @BindView(R.id.refreshImageView)
+    ImageView mRefreshImageView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,17 +83,27 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mProgressBar.setVisibility(View.INVISIBLE);
 
-        final double latitude = 37.8267;
-        final double longitude = -122.423;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
 
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getForecast(latitude, longitude);
+                getForecast(mLatitude, mLongitude);
             }
         });
 
-        getForecast(latitude, longitude);
+        getForecast(mLatitude, mLongitude);
 
 
     }
@@ -67,8 +112,23 @@ public class MainActivity extends AppCompatActivity {
         // create the url that will be used for the http call
         String apiKey = "bf4d36c47381c45990e059d3a65f3a28";
 
+        // TODO: try to get the geocoder to work to get city and state name
+        // hardcode for now
+        if (latitude == 0 && longitude == 0) {
+            latitude = 40.7128;
+            longitude  = -74.0059;
+            mCity = "New York";
+            mState = "NY";
+        } else {
+            mCity = "Nutley";
+            mState = "NJ";
+        }
+
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
+
+
+        Log.i(TAG, "Lat: " + latitude + " // Long: " + longitude);
 
         // check if the network is available before continuing
         if (isNetworkAvailable()) {
@@ -110,15 +170,12 @@ public class MainActivity extends AppCompatActivity {
                                     updateDisplay();
                                 }
                             });
-                        }
-                        else {
+                        } else {
                             alertUserAboutResponseError();
                         }
-                    }
-                    catch (IOException ioe) {
+                    } catch (IOException ioe) {
                         Log.e(TAG, "Exception caught: ", ioe);
-                    }
-                    catch (JSONException jsone) {
+                    } catch (JSONException jsone) {
                         Log.e(TAG, "Exception caught: ", jsone);
                     }
 
@@ -130,8 +187,7 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
             });
-        }
-        else {
+        } else {
             alertUserAboutNetworkError();
         }
     }
@@ -140,8 +196,7 @@ public class MainActivity extends AppCompatActivity {
         if (mProgressBar.getVisibility() == View.INVISIBLE) {
             mProgressBar.setVisibility(View.VISIBLE);
             mRefreshImageView.setVisibility(View.INVISIBLE);
-        }
-        else {
+        } else {
             mProgressBar.setVisibility(View.INVISIBLE);
             mRefreshImageView.setVisibility(View.VISIBLE);
         }
@@ -153,6 +208,7 @@ public class MainActivity extends AppCompatActivity {
         mHumidityValue.setText(String.valueOf(mCurrentWeather.getHumidity()));
         mPrecipValue.setText(mCurrentWeather.getPrecipitation() + "%");
         mSummaryLabel.setText(mCurrentWeather.getSummary());
+        mLocationLabel.setText(mCity + ", " + mState);
 
         Drawable drawable = ContextCompat.getDrawable(this, mCurrentWeather.getIconId());
         mIconImageView.setImageDrawable(drawable);
@@ -211,5 +267,79 @@ public class MainActivity extends AppCompatActivity {
                 getString(R.string.response_error_message),
                 getString(R.string.error_ok_button_text));
         dialog.show(getFragmentManager(), "error_dialog");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            // TODO: I need to figure out how to update the location on the first run of the app
+            mLatitude = 40.7128;
+            mLongitude = -74.0059;
+        } else {
+            handleNewLocation(location);
+        }
+        ;
+    }
+
+    private void handleNewLocation(Location location) {
+        mLatitude = location.getLatitude();
+        mLongitude = location.getLongitude();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
     }
 }
